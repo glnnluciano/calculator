b6dc2a8c27073505c8259091445b155e42c114e0
glnnluciano <glnnluciano@gmail.com>
2024-05-04 21:22:21 +0800

    Pseudocode

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# ask what operation
+# ask for num_1 and num_2
+# print result
+# loop try again
+    # ask the user if wants to try again
+    # if eys, loop
+    # if no, print "Thank You!" and terminate
+# add error handling
