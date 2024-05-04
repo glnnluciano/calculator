e7ab344dce30d6b1cb8c739ab85c8735fbcd0fda
glnnluciano <glnnluciano@gmail.com>
2024-05-04 21:49:22 +0800

    add loop and if/else statement

--- calculator.py
+++ calculator.py
@@ -1,6 +1,14 @@
+while True:    
+    # ask what operation
+    operation = str(input("What operation would you like to perform?\n\nType the word:\n'ADD' for Addition \n'SUB' for Subtraction \n'MUL' for Multiplication \n'DIV' for Division\n\nType Here: ").strip())
+    operation = operation.lower().strip()
 
-# ask what operation
-operation = str(input("What operation would you like to perform?\n\nType the word:\n'ADD' for Addition \n'SUB' for Subtraction \n'MUL' for Multiplication \n'DIV' for Division\n\nType Here: ").strip())
+    operation_list = ["add", "sub", "mul", "div"]
+    if operation in operation_list:
+        break
+            
+    else: 
+        print("\n------------------------------------------------------------\n\nInvalid Input. Please choose from the given options.\n")
 
 # ask for num_1 and num_2
 # print result
