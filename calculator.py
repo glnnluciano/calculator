2a086ad68f1bab9ade6670b59585dda2f64a2ce0
glnnluciano <glnnluciano@gmail.com>
2024-05-04 22:21:32 +0800

    make a def function

--- calculator.py
+++ calculator.py
@@ -1,64 +1,66 @@
-while True:    
-    # ask what operation
-    operation = input("What operation would you like to perform?\n\nType the word:\n'ADD' for Addition \n'SUB' for Subtraction \n'MUL' for Multiplication \n'DIV' for Division\n\nType Here: ")
-    operation = operation.lower().strip()
+def calculator():
+    while True:    
+        # ask what operation
+        operation = input("What operation would you like to perform?\n\nType the word:\n'ADD' for Addition \n'SUB' for Subtraction \n'MUL' for Multiplication \n'DIV' for Division\n\nType Here: ")
+        operation = operation.lower().strip()
 
-    operation_list = ["add", "sub", "mul", "div"]
-    if operation in operation_list:
-        print("\n------------------------------------------------------------\n")
-        break
+        operation_list = ["add", "sub", "mul", "div"]
+        if operation in operation_list:
+            print("\n------------------------------------------------------------\n")
+            break
+                
+        else: 
+            print("\n------------------------------------------------------------\n\nInvalid Input. Please choose from the given options.\n")
+
+    # ask for num_1 and num_2
+    while True:
+        try:  
+
+            num_1 = float(input("Enter first numbers you'd like to operate.\n\nType Here: ").strip())
+            break
+
+        except ValueError:
+            print("\nYour input is NOT A NUMBER. Please try again\n\n------------------------------------------------------------\n")
+
+    while True:   
+        try:
+
+            num_2 = float(input("\nPlease enter second number you'd like to operate.\n\nType Here: ").strip())
+            break
+
+        except ValueError:
+            print("\nYour input is NOT A NUMBER. Please try again\n\n------------------------------------------------------------")
+
+    # print result
+    # print result for addition
+    if operation == "add":
+
+        sum_total = num_1 + num_2        
+
+        print("\n------------------------------------------------------------\n\nThe sum of", num_1, "and", num_2, "is", sum_total)
+
+    # print result for subtraction
+    elif operation == "sub":
+
+        difference = num_1 - num_2
+
+        print("\n------------------------------------------------------------\n\nThe difference of", num_1, "and", num_2, "is", difference)
+
+    #print result for multiplication
+    elif operation == "mul":
+
+        product = num_1 * num_2
+
+        print("\n------------------------------------------------------------\n\nThe product of", num_1, "and", num_2, "is", product)
+
+    #print result for division
+    elif operation == "div":
             
-    else: 
-        print("\n------------------------------------------------------------\n\nInvalid Input. Please choose from the given options.\n")
+            quotient = num_1 / num_2
 
-# ask for num_1 and num_2
-while True:
-    try:  
+            print("\n------------------------------------------------------------\n\nThe quotient of", num_1, "and", num_2, "is", quotient)
 
-        num_1 = float(input("Enter first numbers you'd like to operate.\n\nType Here: ").strip())
-        break
-
-    except ValueError:
-        print("\nYour input is NOT A NUMBER. Please try again\n\n------------------------------------------------------------\n")
-
-while True:   
-    try:
-
-        num_2 = float(input("\nPlease enter second number you'd like to operate.\n\nType Here: ").strip())
-        break
-
-    except ValueError:
-        print("\nYour input is NOT A NUMBER. Please try again\n\n------------------------------------------------------------")
-
-# print result
-# print result for addition
-if operation == "add":
-
-    sum_total = num_1 + num_2        
-
-    print("\n------------------------------------------------------------\n\nThe sum of", num_1, "and", num_2, "is", sum_total)
-
-# print result for subtraction
-elif operation == "sub":
-
-    difference = num_1 - num_2
-
-    print("\n------------------------------------------------------------\n\nThe difference of", num_1, "and", num_2, "is", difference)
-
-#print result for multiplication
-elif operation == "mul":
-
-    product = num_1 * num_2
-
-    print("\n------------------------------------------------------------\n\nThe product of", num_1, "and", num_2, "is", product)
-
-#print result for division
-elif operation == "div":
-        
-        quotient = num_1 / num_2
-
-        print("\n------------------------------------------------------------\n\nThe quotient of", num_1, "and", num_2, "is", quotient)
-
+calculator()
 # loop try again
     # ask the user if wants to try again
     # if eys, loop
