ee0c0cd3051e668a72684f1a2336d3f12a7a587c
glnnluciano <glnnluciano@gmail.com>
2024-05-04 21:35:24 +0800

    input operation

--- calculator.py
+++ calculator.py
@@ -1,4 +1,7 @@
+
 # ask what operation
+operation = str(input("What operation would you like to perform?\n\nType the word:\n'ADD' for Addition \n'SUB' for Subtraction \n'MUL' for Multiplication \n'DIV' for Division\n\nType Here: ").strip())
+
 # ask for num_1 and num_2
 # print result
 # loop try again
